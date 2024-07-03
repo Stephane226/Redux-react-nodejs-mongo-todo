e8e497819a7f0e46feffd5ddce0af8ac65267b3f
Masaaki Stephane <62558026+Stephane226@users.noreply.github.com>
2024-07-03 17:09:01 +0300

    implementing redux
    Actions
    Types
    Reducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import {Provider} from "react-redux";
+import {thunk} from "redux-thunk";
+import { BrowserRouter } from 'react-router-dom';
+
+
+import reportWebVitals from './reportWebVitals';
+import TodoReducer from "./stores/reducer";
+
+const RootReducer = combineReducers({
+  TodoReducer:TodoReducer
+})
+
+// enabled redux to browser 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+//create store
+const store = createStore(RootReducer, composeEnhancers(applyMiddleware(thunk)))
+
+
+ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -3,43 +3,33 @@ import { useSelector, useDispatch } from 'react-redux';
 import { List, ListItem, ListItemText, IconButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 
+import TodoForm from '../components/TodoForm';
+
+
+//redux
+import { Link, Redirect } from "react-router-dom";
+
+
+
+
 const TodoList = () => {
 
-const todos   = [
- {
-  _id : '842989284',
-  description : 'This is a description demo lorem.',
-  status : 'pending'
- },
- {
-  _id : '842989284',
-  description : 'This is a description demo lorem.',
-  status : 'pending'
- },
- {
-  _id : '842989284',
-  description : 'This is a description demo lorem.',
-  status : 'pending'
- },
- {
-  _id : '842989284',
-  description : 'This is a description demo lorem.',
-  status : 'pending'
- },
- {
-  _id : '842989284',
-  description : 'This is a description demo lorem.',
-  status : 'pending'
- },
-]
+
+ const dispatch = useDispatch()
+ const todolist = useSelector(state => state.TodoReducer.todolist);
+
+
+const todos   = []
 
   const handleDelete = (id) => {
    
   };
 
   return (
+   <>
+   <TodoForm/>
     <List style={{marginTop : '50px'}}>
-      {todos.map((todo) => (
+      {todolist && todolist.map((todo) => (
         <ListItem key={todo._id} style={{backgroundColor:'whitesmoke' , marginBottom : '10px',borderRadius : '10px'}}>
           <ListItemText primary={todo.title} secondary={todo.description} />
           <IconButton edge="end" aria-label="delete" onClick={() => handleDelete(todo._id)}>
@@ -48,6 +38,7 @@ const todos   = [
         </ListItem>
       ))}
     </List>
+    </>
   );
 };
 

0cd59e8bcefca02a2c57756ac1ebdde5c5c13841
Masaaki Stephane <62558026+Stephane226@users.noreply.github.com>
2024-07-03 15:08:21 +0300

    todo listing  in TodoList.js

--- frontend/src/components/TodoList.js
+++ frontend/src/components/TodoList.js
@@ -1,12 +1,53 @@
 import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { List, ListItem, ListItemText, IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const TodoList = () => {
 
+const todos   = [
+ {
+  _id : '842989284',
+  description : 'This is a description demo lorem.',
+  status : 'pending'
+ },
+ {
+  _id : '842989284',
+  description : 'This is a description demo lorem.',
+  status : 'pending'
+ },
+ {
+  _id : '842989284',
+  description : 'This is a description demo lorem.',
+  status : 'pending'
+ },
+ {
+  _id : '842989284',
+  description : 'This is a description demo lorem.',
+  status : 'pending'
+ },
+ {
+  _id : '842989284',
+  description : 'This is a description demo lorem.',
+  status : 'pending'
+ },
+]
+
+  const handleDelete = (id) => {
+   
+  };
 
   return (
-   <div>
-    List of all todos...
-   </div>
+    <List style={{marginTop : '50px'}}>
+      {todos.map((todo) => (
+        <ListItem key={todo._id} style={{backgroundColor:'whitesmoke' , marginBottom : '10px',borderRadius : '10px'}}>
+          <ListItemText primary={todo.title} secondary={todo.description} />
+          <IconButton edge="end" aria-label="delete" onClick={() => handleDelete(todo._id)}>
+            <DeleteIcon />
+          </IconButton>
+        </ListItem>
+      ))}
+    </List>
   );
 };
 
